6e2053217fd4e2b2ffe34144117249394864f38f
HyunCafe <eparksuncafe@gmail.com>
2023-04-17 15:54:23 -0500

    Add: added more styling, added simple hamburger menu

--- script.js
+++ script.js
@@ -26,6 +26,13 @@
 /* <------------------------ Dom Events ------------------------> */
 
 
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('navMenu');
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('show-menu');
+  hamburger.classList.toggle('open');
+});
 // About me Type Face
   document.addEventListener("DOMContentLoaded", function () {
     const typed = new Typed("#typed", {
