40c657c6fba0ac6b06791bd6d709fa47c47abe66
HyunCafe <eparksuncafe@gmail.com>
2023-04-22 14:19:51 -0500

    Refactor: Refactor dom events

--- script.js
+++ script.js
@@ -1,62 +1,44 @@
 'use strict';
 
+document.addEventListener('DOMContentLoaded', function() {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* <------------------------ Dom Events ------------------------> */
-
-
-const hamburger = document.getElementById('hamburger');
-const navMenu = document.getElementById('navMenu');
-
-hamburger.addEventListener('click', () => {
-  navMenu.classList.toggle('show-menu');
-  hamburger.classList.toggle('open');
-});
-// About me Type Face
-  document.addEventListener("DOMContentLoaded", function () {
-    const typed = new Typed("#typed", {
-      strings: [
-        "A Jr Front-End Developer",
-        "A Passionate Learner",
-        "Your Next Teammate"
-    ],
-      typeSpeed: 50,
-      backSpeed: 30,
-      loop: true,
-      smartBackspace: true,
-      showCursor: true,
-      backDelay: 2000,
-    });
+  // Hamburger menu
+  const hamburger = document.getElementById('hamburger');
+  const navMenu = document.getElementById('navMenu');
+  
+  hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('show-menu');
+    hamburger.classList.toggle('open');
   });
 
-  document.addEventListener("DOMContentLoaded", function () {
-    const tiltElements = document.querySelectorAll(".projects__item-thumbnail");
-    
-    VanillaTilt.init(tiltElements, {
-      max: 10, 
-      glare: true, 
-      "max-glare": 0.1, 
-    });
+  // Toggle show-menu when clicking outside of navMenu
+  document.addEventListener('click', (event) => {
+    if (!navMenu.contains(event.target) && !hamburger.contains(event.target)) {
+      navMenu.classList.remove('show-menu');
+      hamburger.classList.remove('open');
+    }
   });
   
+  // Typed.js for about me section
+  const typed = new Typed('#typed', {
+    strings: [
+      'A Jr Front-End Developer',
+      'A Passionate Learner',
+      'Your Next Teammate'
+    ],
+    typeSpeed: 50,
+    backSpeed: 30,
+    loop: true,
+    smartBackspace: true,
+    showCursor: true,
+    backDelay: 2000,
+  });
+  
+  // VanillaTilt.js for project thumbnails
+  const tiltElements = document.querySelectorAll('.projects__item-thumbnail');
+  VanillaTilt.init(tiltElements, {
+    max: 10, 
+    glare: true, 
+    'max-glare': 0.1, 
+  });
+});
